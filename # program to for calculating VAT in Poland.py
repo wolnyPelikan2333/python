6ca1e6a36d525655eaf8a0cd96f4c79027176b04
Michal Walewski <pelikan2333@proton.me>
2023-10-05 00:42:33 +0200

    Add part computer

---
+++ # program to for calculating VAT in Poland.py
@@ -0,0 +1,15 @@
+# program  for calculating VAT in Poland
+def main():
+    VAT = 23/100
+JavaCourse = 10000
+PythonCourse = 13600
+vatJavaCourse = JavaCourse * VAT 
+vatPythonCourse = PythonCourse * VAT
+
+monitor = 736 * VAT 
+nvidiaGeforce3090 = 7680 * Vat 
+motheboard = 830 * VAT 
+intel13600k = 1349 * VAT 
+disk  = 450 * VAT 
+case = 735 *VAT 
+
